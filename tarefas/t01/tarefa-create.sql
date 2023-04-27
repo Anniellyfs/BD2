2aaa0a8cb7b42acd7040a6efa9fca48896823112
Annielly <annyfs2015@gmail.com>
2023-04-27 14:01:04 -0300

    criação do SQL

---
+++ tarefas/t01/tarefa-create.sql
@@ -0,0 +1,5 @@
+funcionario(codigo, nome, sexo, dt_nasc, salario, cod_depto)
+departamento(codigo, descricao, cod_gerente)
+projeto (codigo, nome, descricao, cod_depto, cod_responsavel, data_inicio, data_fim)
+atividade(codigo, nome, descricao, cod_responsavel, data_inicio, data_fim)
+atividade_projeto(cod_projeto, cod_atividade)
