9de52446f303c23f5aae2b52ce22185db6e7b5b8
Annielly <annyfs2015@gmail.com>
2023-04-27 14:37:04 -0300

    criação de tabelas

--- tarefas/t01/tarefa-create.sql
+++ tarefas/t01/tarefa-create.sql
@@ -1,5 +1,41 @@
-funcionario(codigo, nome, sexo, dt_nasc, salario, cod_depto)
-departamento(codigo, descricao, cod_gerente)
-projeto (codigo, nome, descricao, cod_depto, cod_responsavel, data_inicio, data_fim)
-atividade(codigo, nome, descricao, cod_responsavel, data_inicio, data_fim)
-atividade_projeto(cod_projeto, cod_atividade)
+CREATE TABLE funcionario (
+	codigo SERIAL PRIMARY KEY,
+	nome VARCHAR(50),
+	sexo CHAR(1),
+	dtNasc DATE,
+	salario DECIMAL(10,2),
+	codSupervisor INT,
+	codDepto INT,
+	FOREIGN KEY (codSupervisor) REFERENCES funcionario(codigo) ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+CREATE TABLE departamento (
+	codigo SERIAL PRIMARY KEY,
+	sigla VARCHAR(10),
+	descricao VARCHAR(50),
+	codGerente INT,
+	UNIQUE (sigla),
+	FOREIGN KEY (codGerente) REFERENCES funcionario(codigo) ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+CREATE TABLE projeto (
+	codigo SERIAL PRIMARY KEY,
+	nome VARCHAR(50),
+	descricao VARCHAR(250),
+	codResponsavel INT,
+	codDepto INT,
+	dataInicio DATE, 
+	dataFim DATE,
+	UNIQUE (nome),
+	FOREIGN KEY (codResponsavel) REFERENCES funcionario(codigo) ON DELETE SET NULL ON UPDATE CASCADE,
+	FOREIGN KEY (codDepto) REFERENCES departamento(codigo) ON DELETE SET NULL ON UPDATE CASCADE
+);
+ 
+CREATE TABLE atividade (
+	codigo SERIAL PRIMARY KEY,
+	descricao VARCHAR(250),
+	codProjeto INT,
+	dataInicio DATE, 
+	dataFim DATE,
+	FOREIGN KEY (codProjeto) REFERENCES projeto(codigo) ON DELETE SET NULL ON UPDATE CASCADE
+);
