ad1ca5e12b53821c162bc0aa5f26d98a2daafc08
Annielly <annyfs2015@gmail.com>
2023-05-01 11:46:20 -0300

    resolução de tarefas #1

--- tarefas/t01/scripts/tarefa01-q14.sql
+++ tarefas/t01/scripts/tarefa01-q14.sql
@@ -4,12 +4,14 @@ o número de atividades que este funcionário é responsável. Crie e use views 
 
 
 -- Criação da view para contar o número de atividades por funcionário
+
 CREATE VIEW atividades_por_funcionario AS
 SELECT cod_responsavel, COUNT(*) AS num_atividades
 FROM atividade
 GROUP BY cod_responsavel;
 
 -- Consulta para obter o nome dos funcionários responsáveis por atividades e o número de atividades que cada funcionário é responsável
+
 SELECT f.nome AS nome_funcionario, COALESCE(a.num_atividades, 0) AS numero_atividades
 FROM funcionario f
 LEFT JOIN atividades_por_funcionario a ON f.codigo = a.cod_responsavel;
