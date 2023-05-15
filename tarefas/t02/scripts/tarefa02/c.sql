4ae6290d8ddeff3c4e2557c71aaac2752466ccf8
Annielly <annyfs2015@gmail.com>
2023-05-15 16:02:47 -0300

    resolução questão c #2

--- tarefas/t02/scripts/tarefa02/c.sql
+++ tarefas/t02/scripts/tarefa02/c.sql
@@ -1,3 +1,24 @@
 /*Faça um procedimento para exibir os funcionários com idade acima da média do seu
 departamento.*/
 
+CREATE OR REPLACE FUNCTION exibir_funcionarios_acima_media() RETURNS TABLE (
+    codigo INT,
+    nome VARCHAR(15),
+    idade INT
+) AS $$
+BEGIN
+    RETURN QUERY
+    SELECT f.codigo, f.nome, EXTRACT(YEAR FROM age(current_date, f.dataNasc))::INT AS idade
+    FROM funcionario f
+    INNER JOIN departamento d ON f.depto = d.codigo
+    INNER JOIN (
+        SELECT depto, AVG(EXTRACT(YEAR FROM age(current_date, dataNasc))) AS media_idade
+        FROM funcionario
+        GROUP BY depto
+    ) AS avg_age ON f.depto = avg_age.depto
+    WHERE EXTRACT(YEAR FROM age(current_date, f.dataNasc)) > avg_age.media_idade::INT
+    GROUP BY f.codigo, f.nome, idade;
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT * FROM exibir_funcionarios_acima_media();
